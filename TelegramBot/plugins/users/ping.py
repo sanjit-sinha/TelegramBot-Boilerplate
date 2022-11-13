274de8f45c1a89afed9c5b8f28cc8fca2726a5f2
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-11-13 22:45:53 +0530

    Update ping.py

--- TelegramBot/plugins/users/ping.py
+++ TelegramBot/plugins/users/ping.py
@@ -1,26 +1,22 @@
-from TelegramBot.helpers.functions import get_readable_time
-from pyrogram import filters, Client
-from TelegramBot import BotStartTime
+from pyrogram import Client, filters
+from pyrogram.types import Message
 from TelegramBot.config import *
 from datetime import datetime
 import requests as r
-import time
 
 prefixes = COMMAND_PREFIXES
-commands = ["alive", f"alive@{BOT_USERNAME}", "ping", f"ping@{BOT_USERNAME}"]
+commands = ["ping", f"ping@{BOT_USERNAME}"]
 
 
 @Client.on_message(filters.command(commands, **prefixes))
-async def alive(_, message):
-    """
-    To check if bot is alive or not with bot uptime message and ping speed of telegram API.
-    """
-    
-    botuptime = get_readable_time(time.time() - BotStartTime)
-    
-    start = datetime.now()
-    r.get("http://api.telegram.org")
-    end = datetime.now()
-    pong = (end - start).microseconds / 1000
-    
-    await message.reply_text(f"**Bot is alive since {botuptime}. ping Time: `{pong}`ms**", quote=True)   
+async def ping(_, message: Message):
+   """
+   Checks ping speed of Telegram API.
+   """
+
+   start = datetime.now()
+   r.get("http://api.telegram.org")
+   end = datetime.now()
+
+   pong = (end - start).microseconds / 1000
+   await message.reply_text(f"**PONG!!** | Ping Time: `{pong}`ms", quote=True)
