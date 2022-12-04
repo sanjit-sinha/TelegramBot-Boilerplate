94a8e09c93995165c753243248a215a8b379f62d
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-05 03:00:15 +0530

    Update functions.py

--- TelegramBot/helpers/functions.py
+++ TelegramBot/helpers/functions.py
@@ -22,11 +22,9 @@ async def isAdmin(message: Message) -> bool:
         return True
     elif check_status.status in [
         ChatMemberStatus.OWNER,
-        ChatMemberStatus.ADMINISTRATOR,
-    ]:
+        ChatMemberStatus.ADMINISTRATOR]:
         return True
-    else:
-        return False
+    else: return False
 
 
 def get_readable_time(seconds: int) -> str:
@@ -39,20 +37,20 @@ def get_readable_time(seconds: int) -> str:
     days = int(days)
 
     if days != 0:
-        result += f"{days}d"
+        result += f"{days}d "
     (hours, remainder) = divmod(remainder, 3600)
     hours = int(hours)
 
     if hours != 0:
-        result += f"{hours}h"
+        result += f"{hours}h "
     (minutes, seconds) = divmod(remainder, 60)
     minutes = int(minutes)
 
     if minutes != 0:
-        result += f"{minutes}m"
+        result += f"{minutes}m "
 
     seconds = int(seconds)
-    result += f"{seconds}s"
+    result += f"{seconds}s "
     return result
 
 
