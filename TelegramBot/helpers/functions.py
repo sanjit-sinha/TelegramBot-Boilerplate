64a528e6e779de18f2c231c33ad7279a2c360675
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-10 23:44:51 +0530

    Update functions.py

--- TelegramBot/helpers/functions.py
+++ TelegramBot/helpers/functions.py
@@ -58,15 +58,16 @@ def get_readable_bytes(size):
 	"""
 	Return a human readable file size from bytes.
 	"""
-
-    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
-  
-    if not size: return ""
-    power = 2**10
-    raised_to_pow = 0
-    
-    while size > power:
-        size /= power
-        raised_to_pow += 1
-        
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+	
+	dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+	
+	if not size: return ""
+	power = 2**10
+	raised_to_pow = 0
+	
+	while size > power:
+	    size /= power
+	    raised_to_pow += 1
+	    
+	return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+	
