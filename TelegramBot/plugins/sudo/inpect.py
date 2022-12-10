ec962b355d197ef06950166779ecddcc139ed473
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-10 20:29:06 +0530

    Update inpect.py

--- TelegramBot/plugins/sudo/inpect.py
+++ TelegramBot/plugins/sudo/inpect.py
@@ -13,6 +13,8 @@ async def inspect(_, message: Message):
 	"""
 	isnpect the message and give reply in json format.
 	"""
+	
+	
 	try:
 	   await message.reply_text(message, quote=True)
 	except MessageTooLong:
