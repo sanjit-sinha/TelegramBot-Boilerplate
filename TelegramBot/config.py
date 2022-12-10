dcc3cd28ff4d1179cb4dd1d66950abc493a340cf
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-10 23:37:29 +0530

    Update config.py

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -3,19 +3,14 @@ from os import getenv
 from dotenv import load_dotenv
 load_dotenv("config.env") 
 
+
+
 COMMAND_PREFIXES = dict(prefixes=json.loads(getenv("COMMAND_PREFIXES")))
-
 OWNER_USERID = json.loads(getenv("OWNER_USERID"))
-SUDO_USERID = OWNER_USERID #owner is ultimately sudo user too
-
-try:
-  SUDO_USERID += json.loads(getenv("SUDO_USERID")) 
+SUDO_USERID = OWNER_USERID #
+try: SUDO_USERID += json.loads(getenv("SUDO_USERID")) 
 except: pass
-
-BOT_USERNAME = getenv("BOT_USERNAME")
-BOT_TOKEN = getenv("BOT_TOKEN")
-
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
 
-if "@" in BOT_USERNAME: BOT_USERNAME = BOT_USERNAME.replace("@", "")
