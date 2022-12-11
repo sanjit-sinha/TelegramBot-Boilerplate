b331e1927385d4f21068321ebbd4d9737aa5f0c7
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-12 03:16:44 +0530

    Create start_constants.py

---
+++ TelegramBot/assets/start_constants.py
@@ -0,0 +1,52 @@
+from TelegramBot.version import (__python_version__, __version__, __pyro_version__,  __license__)
+
+START_ANIMATION = "https://telegra.ph/file/c0857672b427bec8542f6.mp4"
+
+START_CAPTION = """**Hey there!! I am simple TelegramBot wich is made for the purpose for trying, testing, deploying and learnig about telegram bot using python pyrogram framework. \n\n Use buttons to navigate and know more about me :)**"""
+
+COMMAND_CAPTION = """**Here are the list of commands wich you can use in bot.\n**"""
+
+ABOUT_CAPTION = f"""• Python version : {__python_version__}
+• Bot version : {__version__}
+• pyrogram  version : {__pyro_version__}
+• License : {__license__}
+
+**Github Repo**: https://github.com/sanjit-sinha/Telegram-Bot-Boilerplate"""
+
+USER_TEXT = """🗒️ Documentation for commands available to user's 
+	
+• /start: To Get this message
+	
+• /help: Alias command for start
+	
+• /alive: To check if bot is alive or not.
+
+• /ping: Alias command for alive.
+
+• /paste: paste text to katb.in website.
+"""
+
+SUDO_TEXT = """
+🗒️ Documentation for Sudo Users commands.
+
+• /speedtest: Check the internet speed of bot server.
+
+• /serverstats: Get the stats of server.
+
+• /stats: alias command for serverstats
+
+• /log: To get the log file of bot.
+"""
+
+DEV_TEXT = """
+🗒️ Documentation for Developers Commands.
+	
+• /update: To update the bot to latest commit from repository. 
+
+• /restart: Restart the bot.
+
+• /shell: To run the terminal commands via bot.
+
+• /py: To run the python commands via bot
+"""
+
