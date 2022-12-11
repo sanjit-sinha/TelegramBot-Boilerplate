c39a82569b34d91838447e91e067b8bc9b41e259
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-11 21:07:46 +0530

    Update ping.py

--- TelegramBot/plugins/users/ping.py
+++ TelegramBot/plugins/users/ping.py
@@ -8,9 +8,6 @@ import time
 import httpx
 import asyncio
 
-async def test():
-	time.sleep(10)
-	print("Ok")
 	
 
 commands = ["ping", "alive"]
@@ -26,8 +23,6 @@ async def ping(_, message: Message):
    async with httpx.AsyncClient() as client:
      	await client.get("http://api.telegram.org") 	
    end = datetime.now()
-   await asyncio.ensure_future(test())
-
    
    botuptime = get_readable_time(time.time() - BotStartTime)
    pong = (end - start).microseconds / 1000
