64df994d521c303867574a1811094b5df5bcd60c
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-12 04:22:46 +0530

    Update __init__.py

--- TelegramBot/__init__.py
+++ TelegramBot/__init__.py
@@ -1,18 +1,20 @@
+from asyncio import get_event_loop, new_event_loop, set_event_loop
 from TelegramBot.config import BOT_TOKEN, API_ID, API_HASH
-from TelegramBot.logging import LOGGER, logging
+from TelegramBot.logging import LOGGER
 from pyrogram import Client
 import time
 import sys
 
+LOGGER(__name__).info("Starting TelegramBot....")
 BotStartTime = time.time()
-plugins = dict(root="TelegramBot/plugins")
 
-if sys.version_info[0] < 3 or sys.version_info[1] < 7:
-    VERSION_ASCII = """
+VERSION_ASCII ="""
   =============================================================
-  "You MUST need to be on python 3.7 or above, shutting down the bot...
+  You MUST need to be on python 3.7 or above, shutting down the bot...
   =============================================================
   """
+  
+if sys.version_info[0] < 3 or sys.version_info[1] < 7:
     LOGGER(__name__).critical(VERSION_ASCII)
     sys.exit(1)
 
@@ -26,19 +28,26 @@ ____________________________________________________________________
 |    | |  __/ |  __/ (_| | | | (_| | | | | | | |_) | (_) | |_      |
 |    |_|\___|_|\___|\__, |_|  \__,_|_| |_| |_|____/ \___/ \__|     |
 |                    __/ |                                         |
-|____________________________________________________________________   
+|__________________________________________________________________|   
 """
+
 # https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+
+
+try:
+	loop = get_event_loop()
+except RuntimeError:
+	set_event_loop(new_event_loop())
+	loop = get_event_loop()
 
 LOGGER(__name__).info(BANNER)
 LOGGER(__name__).info("initiating the client")
 
-LOGGER("pyrogram").setLevel(logging.WARNING)
 
+plugins = dict(root="TelegramBot/plugins")
 bot = Client(
     "TelegramBot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=plugins,
-)  # https://docs.pyrogram.org/topics/smart-plugins
+    plugins=plugins)  #https://docs.pyrogram.org/topics/smart-plugins
