1f2a77ab1d024911e5f98e5b6735f0ef364c5a8e
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-15 00:28:27 +0530

    Improved Decorators. (addition of functools wraps)

--- TelegramBot/helpers/decorators.py
+++ TelegramBot/helpers/decorators.py
@@ -1,21 +1,23 @@
 """
-About python decorators.
+More about python decorators.
 https://www.geeksforgeeks.org/decorators-in-python/
 https://realpython.com/primer-on-python-decorators/
 """
 
 from TelegramBot.helpers.functions import isAdmin
+from pyrogram.types import Message
+from TelegramBot.config import *
+from functools import wraps
 from pyrogram import Client
 from typing import Callable
-from TelegramBot.config import *
-from pyrogram.types import Message
 
 
 def sudo_commands(func: Callable) -> Callable:
     """
     Restricts user's from executing certain sudo user's' related commands
     """
-
+    
+    @wraps(func)
     async def decorator(client: Client, message: Message):
         if message.from_user.id in SUDO_USERID:
             return await func(client, message)
@@ -27,7 +29,8 @@ def dev_commands(func: Callable) -> Callable:
     """
     Restricts user's from executing certain developer's related commands
     """
-
+    
+    @wraps(func)
     async def decorator(client: Client, message: Message):
         if message.from_user.id in OWNER_USERID:
             return await func(client, message)
@@ -39,7 +42,8 @@ def admin_commands(func: Callable) -> Callable:
     """
     Restricts user's from using group admin commands.
     """
-
+    
+    @wraps(func)
     async def decorator(client: Client, message: Message):
         if await isAdmin(message):
             return await func(client, message)
@@ -51,7 +55,8 @@ def errors(func: Callable) -> Callable:
     """
     Try and catch error of any function.
     """
-
+    
+    @wraps(func)
     async def decorator(client: Client, message: Message):
         try:
             return await func(client, message)
