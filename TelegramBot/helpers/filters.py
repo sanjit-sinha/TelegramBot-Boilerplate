cbfbe57c72b4a642345ce22ffa69f17632125dcb
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-29 23:22:15 +0530

    removed filters template.

--- TelegramBot/helpers/filters.py
+++
@@ -1,6 +0,0 @@
-"""
-Boilerplate file for creating custom filters wich can be further imported in other file(s).
-for example: 
-
-filters_one = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
-"""
