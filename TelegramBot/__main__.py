9ac38128cf601016fc10ddc2a1d74b39a55fd7b8
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-12-30 04:45:42 +0530

    Update __main__.py

--- TelegramBot/__main__.py
+++ TelegramBot/__main__.py
@@ -1,7 +1,7 @@
 from TelegramBot import bot
 from TelegramBot.logging import LOGGER
 
-LOGGER(__name__).info("client successfully initiated. ")
+LOGGER(__name__).info("client successfully initiated....")
 if __name__ == "__main__":
     bot.run()
 
