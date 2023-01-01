3e695eb6214b80f543d998525e3555faaf166348
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-01 10:04:52 +0530

    Update decorators.py

--- TelegramBot/helpers/decorators.py
+++ TelegramBot/helpers/decorators.py
@@ -4,17 +4,52 @@ https://www.geeksforgeeks.org/decorators-in-python/
 https://realpython.com/primer-on-python-decorators/
 """
 
+from pyrogram.types import Message, CallbackQuery
 from TelegramBot.helpers.functions import isAdmin
-from pyrogram.types import Message
+from TelegramBot.helpers.ratelimiter import RateLimiter 
 from TelegramBot.config import *
+from typing import Callable, Union
+from cachetools import TTLCache
 from functools import wraps
 from pyrogram import Client
-from typing import Callable
 
 
+ratelimiter = RateLimiter()
+warned_users = TTLCache(maxsize=128, ttl=600)
+warning_message = "Spam detected! ignoring your all requests for few minutes."
+    
+
+def ratelimit(func: Callable) -> Callable:
+    """
+    Restricts user's from spamming commands or pressing buttons multiple times
+    using leaky bucket algorithm and pyrate_limiter.
+    """
+    
+    @wraps(func)
+    async def decorator(client: Client, update: Union[Message, CallbackQuery]):
+	    userid = update.from_user.id
+	    is_limited = ratelimiter.acquire(userid) ; print(warned_users)
+	    
+	    if is_limited and userid not in warned_users:
+	    	if isinstance(update, Message):
+	    		await update.reply_text(warning_message)
+	    		warned_users[userid] = 1
+	    		return
+	    	
+	    	elif isinstance(update, CallbackQuery):
+	    		await update.answer(warning_message, show_alert=True)
+	    		warned_users[userid] = 1
+	    		return
+	    
+	    elif is_limited and userid in warned_users: pass
+	    else: return await func(client, update)
+   
+    return decorator
+    
+    
 def sudo_commands(func: Callable) -> Callable:
     """
-    Restricts user's from executing certain sudo user's' related commands
+    Restricts user's from executing certain sudo user's' related commands.
     """
     
     @wraps(func)
@@ -27,7 +62,7 @@ def sudo_commands(func: Callable) -> Callable:
 
 def dev_commands(func: Callable) -> Callable:
     """
-    Restricts user's from executing certain developer's related commands
+    Restricts user's from executing certain developer's related commands.
     """
     
     @wraps(func)
