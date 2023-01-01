871cfafdce2e82eb42498445be97e2cc0a658931
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-01 10:11:00 +0530

    Update inpect.py

--- TelegramBot/plugins/sudo/inpect.py
+++ TelegramBot/plugins/sudo/inpect.py
@@ -1,5 +1,5 @@
 from TelegramBot.helpers.pasting_services import katbin_paste
-from TelegramBot.helpers.decorators import sudo_commands
+from TelegramBot.helpers.decorators import sudo_commands, ratelimiter
 from pyrogram.errors import MessageTooLong
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -9,6 +9,7 @@ from TelegramBot.config import *
 commands = ["inspect"]	
 @Client.on_message(filters.command(commands, **prefixes))
 @sudo_commands
+@ratelimiter
 async def inspect(client: Client, message: Message):
   """
   isnpect the message and give reply in json format.
