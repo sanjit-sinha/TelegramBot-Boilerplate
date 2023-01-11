89791c95ce8aa1d7ab32a0ae3200011624051ce7
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-11 21:14:39 +0530

    Update dbstats.py

--- TelegramBot/plugins/sudo/dbstats.py
+++ TelegramBot/plugins/sudo/dbstats.py
@@ -1,16 +1,16 @@
+from pyrogram.types import Message 
 from pyrogram import Client, filters
 
-from TelegramBot.config import prefixes
 from TelegramBot.database import MongoDb
-from TelegramBot.helpers.decorators import ratelimiter, sudo_commands
-
-commands = ["dbstats"]
+from TelegramBot.helpers.decorators import ratelimiter
 
 
-@Client.on_message(filters.command(commands, **prefixes))
-@sudo_commands
+@Client.on_message(filters.command(["dbstats"]) & SUDO_USERS)
 @ratelimiter
 async def dbstats(_, message):
+    """
+    Returns database stats of MongoDB.
+    """ 
     TotalUsers = await MongoDb.users.total_documents()
     TotalChats = await MongoDb.chats.total_documents()
     stats_string = f"**Bot Database Statics.\n\n**Total Number of users = {TotalUsers}\nTotal number of chats  = {TotalChats}"
