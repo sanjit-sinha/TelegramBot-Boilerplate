7eb88270245e66b1c7efcc420ec411c1fb3b259d
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-11 20:46:50 +0530

    Update pasting_services.py

--- TelegramBot/helpers/pasting_services.py
+++ TelegramBot/helpers/pasting_services.py
@@ -7,6 +7,7 @@ async def katbin_paste(text: str) -> str:
     """
     paste the text in katb.in website.
     """
+    
     katbin_url = "https://katb.in"
     client = AsyncClient()
     response = await client.get(katbin_url)
@@ -16,8 +17,7 @@ async def katbin_paste(text: str) -> str:
         paste_post = await client.post(
             katbin_url,
             data={"_csrf_token": csrf_token, "paste[content]": text},
-            follow_redirects=False,
-        )
+            follow_redirects=False)
         output_url = f"{katbin_url}{paste_post.headers['location']}"
         await client.aclose()
         return output_url
@@ -29,6 +29,7 @@ async def telegraph_paste(content: str, title="TelegramBot") -> str:
     """
     paste the text in telegra.ph (graph.org) website.
     """
+    
     telegraph = Telegraph(domain="graph.org")
     await telegraph.create_account(short_name=title)
     html_content = "<pre>" + content.replace("\n", "<br>") + "</pre>"
@@ -37,8 +38,7 @@ async def telegraph_paste(content: str, title="TelegramBot") -> str:
         response = response["url"]
     except:
         response = await katbin_paste(content)
-    try:
-        telegraph.revoke_access_token()
-    except:
-        pass
+        
+    try: telegraph.revoke_access_token()
+    except: pass
     return response
