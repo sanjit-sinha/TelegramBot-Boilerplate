1890e49738caf1b5654114e2c4c8117c28474531
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-11 20:50:00 +0530

    Update paste.py

--- TelegramBot/plugins/users/paste.py
+++ TelegramBot/plugins/users/paste.py
@@ -3,37 +3,39 @@ from os import remove
 import aiofiles
 from pyrogram import Client, filters
 
-from TelegramBot.config import prefixes
 from TelegramBot.helpers.decorators import ratelimiter
 from TelegramBot.helpers.pasting_services import katbin_paste
 
-commands = ["paste"]
-paste_usage = f"**Usage:** paste the text to katb.in website. Reply to a text file, text message or just type the text after commamd.\n\n**Example:** /paste type your text"
 
-
-@Client.on_message(filters.command(commands, **prefixes))
+@Client.on_message(filters.command(["paste"]))
 @ratelimiter
 async def paste(_, message):
     """
     Paste the text to katb.in website.
     """
+    
+    paste_usage = f"**Usage:** paste the text to katb.in website. Reply to a text file, text message or just type the text after commamd.\n\n**Example:** /paste type your text"
     replied_message = message.reply_to_message
+    
     if len(message.command) > 1:
         content = message.text.split(None, 1)[1]
+        
     elif replied_message:
         if replied_message.text:
             content = replied_message.text
+            
         elif replied_message.document and any(
-            format in replied_message.document.mime_type for format in ["text", "json"]
-        ):
+            format in replied_message.document.mime_type for format in ["text", "json"]):
             await message.reply_to_message.download("temp_file")
             async with aiofiles.open("temp_file", "r+") as file:
                 content = await file.read()
             remove("temp_file")
         else:
             return await message.reply_text(paste_usage, quote=True)
+        
     elif len(message.command) < 2:
         return await message.reply_text(paste_usage, quote=True)
+    
     paste_reply = await message.reply_text("pasting...", quote=True)
     output = await katbin_paste(content)
     return await paste_reply.edit(f"{output}", disable_web_page_preview=True)
