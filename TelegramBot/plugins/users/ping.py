eb2488c0a0553b534eef9bad1b76c6c44af195df
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-11 21:00:10 +0530

    Update ping.py

--- TelegramBot/plugins/users/ping.py
+++ TelegramBot/plugins/users/ping.py
@@ -1,29 +1,29 @@
 from time import time
 
 from httpx import AsyncClient
-from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram import Client, filters 
 
 from TelegramBot import BotStartTime
-from TelegramBot.config import prefixes
 from TelegramBot.helpers.decorators import ratelimiter
 from TelegramBot.helpers.functions import get_readable_time
 
-commands = ["ping", "alive"]
 
-
-@Client.on_message(filters.command(commands, **prefixes))
+@Client.on_message(filters.command(["ping", "alive"]))
 @ratelimiter
-async def ping(_, message):
+async def ping(_, message: Message):
     """
     Give ping speed of Telegram API along with Bot Uptime.
     """
+    
     pong_reply = await message.reply_text("pong!", quote=True)
+    
     start = time()
     async with AsyncClient() as client:
         await client.get("http://api.telegram.org")
     end = time()
+    
     botuptime = get_readable_time(time() - BotStartTime)
     pong = (end - start) / 1000
     return await pong_reply.edit(
-        f"**Ping Time:** `{pong}`ms | **Bot is alive since:** `{botuptime}`"
-    )
+        f"**Ping Time:** `{pong}`ms | **Bot is alive since:** `{botuptime}`")
