14842282ab1d73dee81725c51b9b007bf7e163c9
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-12 08:01:11 +0530

    Update filters.py

--- TelegramBot/helpers/filters.py
+++ TelegramBot/helpers/filters.py
@@ -1,1 +1,25 @@
+"""
+Creating custom filters 
+https://docs.pyrogram.org/topics/create-filters
+"""
 
+from pyrogram import filters as
+from pyrogram.types import Message
+
+from TelegramBot.config import SUDO_USERID,  OWNER_USERID 
+
+
+def dev_users(_, __, message: Message) -> bool:
+    if not message.from_user:
+        return False
+    return message.from_user.id == OWNER_USERID
+    
+    
+def sudo_users(_, __, message: Message) -> bool:
+    if not message.from_user:
+        return False
+    return message.from_user.id == SUDO_USERID 
+
+
+dev_cmd  = filters.create(dev_users)
+sudo_cmd = filters.create(sudo_users)
