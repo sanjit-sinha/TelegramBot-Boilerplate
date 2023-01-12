ae064ee9aee79eb770eed78094973d0eb17553b5
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-12 10:01:35 +0530

    Update filters.py

--- TelegramBot/helpers/filters.py
+++ TelegramBot/helpers/filters.py
@@ -8,11 +8,15 @@ from pyrogram.types import Message
 from TelegramBot.config import SUDO_USERID,  OWNER_USERID 
 
 
-def dev_users(_, __, message: Message) -> bool:   
+def dev_users(_, __, message: Message) -> bool:
+    if not message.from_user:
+    	return False
     return message.from_user.id in OWNER_USERID
     
     
-def sudo_users(_, __, message: Message) -> bool: 
+def sudo_users(_, __, message: Message) -> bool:
+    if not message.from_user:
+    	return False
     return message.from_user.id in SUDO_USERID 
 
 
