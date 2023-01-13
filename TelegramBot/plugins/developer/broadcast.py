3ba7d007874c5b353ac90a71c537d17f6a3069a9
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-14 05:13:58 +0530

    Update broadcast.py

--- TelegramBot/plugins/developer/broadcast.py
+++ TelegramBot/plugins/developer/broadcast.py
@@ -1,4 +1,4 @@
-from asyncio import sleep, gather
+from asyncio import sleep 
 
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
@@ -18,13 +18,11 @@ async def broadcast(client: Client, message: Message):
     """
 
     if not (broadcast_msg := message.reply_to_message):
-        broadcast_usage = f"**Usage:** Broadcast the message to all users as well as chats witch are saved in database.\n\n/broadcast type your message\n\nuse the flag '-all' to send broadcast message to both users as well as chats.\n\n/broadcast -all type your message."
+        broadcast_usage = f"**Usage:** Broadcast the message to all users as well as chats which are saved in database.\n\n/broadcast type your message\n\nuse the flag '-all' to send broadcast message to both users as well as chats.\n\n/broadcast -all type your message."
         return await message.reply_text(broadcast_usage, quote=True)
 
     proses_msg = await message.reply_text(
-        "**Broadcasting started. Please wait for few minutes for it to get completed.",
-        quote=True,
-    )
+        "**Broadcasting started. Please wait for few minutes for it to get completed.", quote=True)
 
     to_chats = False
     to_users = False
@@ -33,6 +31,7 @@ async def broadcast(client: Client, message: Message):
 
     if len(commands) > 3:
         return await proses_msg.edit("Invalid Command")
+         
     for command in message.command:
         if command.lower() == "all":
             to_chats = True
@@ -52,36 +51,25 @@ async def broadcast(client: Client, message: Message):
 
     if not all([to_users, to_chats]):
         to_users = True
+        
     if to_users:
         total_list += await MongoDb.users.get_all_id()
-
-    list_f = [
-        _sending_broadcast(broadcast_msg, disable_notification, __id)
-        for __id in total_list
-    ]
-    tasks = gather(*list_f, return_exceptions=True)
-    results = await tasks
-    success = 0
+        
     failed = 0
-    for result in results:
-        if result == "success":
+    success = 0
+        
+    for __id in total_list:
+        try:
+            await broadcast_msg.copy(
+            __id, broadcast_msg.caption, disable_notification=disable_notification)
             success += 1
-        else:
-            failed += 1
+            
+            #preventing flood wait
+            sleep(0.2)
+        except Exception as error:
+        	LOGGER(__name__).error(str(error))
+        	failed += 1
+        	
     return await proses_msg.edit(
-        f"**The message has been successfully broadcasted.**\n\nTotal success = {success}\nTotal Failure = {failed}"
-    )
+        f"**The message has been successfully broadcasted.**\n\nTotal success = {success}\nTotal Failure = {failed}")
 
-
-async def _sending_broadcast(broadcast_msg, disable_notification, __id):
-    try:
-        await broadcast_msg.copy(
-            __id, broadcast_msg.caption, disable_notification=disable_notification
-        )
-        return "success"
-    except FloodWait as flood:
-        await sleep(flood.value)
-        return await _sending_broadcast(broadcast_msg, disable_notification, __id)
-    except Exception as e:
-        LOGGER(__name__).error(str(e))
-        return "failed"
