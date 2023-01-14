49d5e9d0e1a139922573ac6809d9c9f44af50fcb
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-01-14 14:12:28 +0530

    Fixed exec refresh button.

--- TelegramBot/plugins/developer/terminal.py
+++ TelegramBot/plugins/developer/terminal.py
@@ -57,96 +57,84 @@ async def aexec(code, client, message):
 
 
 async def py_runexec(client: Client, message: Message, replymsg: Message):
-
+     
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
     redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
-
+    
     try:
-        await replymsg.edit("executing...")
-        code = message.text.split(maxsplit=1)[1]
+    	await replymsg.edit("executing...") 	    
+    	code = message.text.split(None, 1)[1]  	
     except IndexError:
-        return await replymsg.edit(
-            "No codes found to execute.",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Refresh  🔄", callback_data="refresh")]]
-            ),
-        )
-
+    	return await replymsg.edit("No codes found to execute.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Refresh  🔄", callback_data="refresh")]]))
+    	
     if "config.env" in code:
-        return await replymsg.edit(
-            "That's a dangerous operation! Not Permitted!",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Refresh  🔄", callback_data="refresh")]]
-            ),
-        )
-
+        return await replymsg.edit("That's a dangerous operation! Not Permitted!", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Refresh  🔄",  callback_data="refresh")]]))
+                                            
     try:
-        await aexec(code, client, message)
+    	await aexec(code, client, message)
     except Exception:
-        exc = traceback.format_exc()
+    	exc = traceback.format_exc()
 
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-
+ 
     evaluation = ""
     if exc:
-        evaluation = exc
+    	evaluation = exc
     elif stderr:
         evaluation = stderr
     elif stdout:
         evaluation = stdout
     else:
-        evaluation = "success"
-    final_output = f"{evaluation.strip()}"
+        evaluation = "success" 
+    final_output = f"{evaluation.strip()}"    
 
-    if len(final_output) <= 4000:
-        return await replymsg.edit(
-            final_output,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("refresh 🔄", callback_data="refresh")]]
-            ),
-        )
-    await replymsg.edit(
-        "output too large. sending it as a file...",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("refresh 🔄", callback_data="refresh")]]
-        ),
-    )
-    file = BytesIO(evaluation.encode())
-    file.name = "output.txt"
-    await replymsg.reply_document(file, caption=file.name, quote=True)
+        
+    if len(final_output) > 4000:
+        async with aiofiles.open("output.txt", "w+", encoding="utf8") as file:
+            await file.write(str(evaluation.strip()))
+       
+        await replymsg.edit("output too large. sending it as a file...",  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("refresh 🔄",callback_data="refresh")]]))
+        await client.send_document(message.chat.id, "output.txt", caption="output.txt")
+        os.remove("output.txt")
+    
+    else:
+    	return await replymsg.edit(final_output,  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("refresh 🔄",callback_data="refresh")]]))
 
 
 @Client.on_callback_query(filters.regex("refresh"))
 @ratelimiter
 async def pyCallbacks(client, CallbackQuery: CallbackQuery):
-    clicker_user_id = CallbackQuery.from_user.id
+    
+    cliker_user_id = CallbackQuery.from_user.id
     message_user_id = CallbackQuery.message.reply_to_message.from_user.id
-    if clicker_user_id != message_user_id:
-        return await CallbackQuery.answer(
-            "That command is not initiated by you.", show_alert=True)
-                   
-    await CallbackQuery.answer()
+    
+    if cliker_user_id != message_user_id:
+    	return await CallbackQuery.answer("That command is not initiated by you.", show_alert=True )
+
+    message  = await client.get_messages(
+	    CallbackQuery.message.chat.id,CallbackQuery.message.reply_to_message.id)	    
+    replymsg = await client.get_messages(
+	    CallbackQuery.message.chat.id, CallbackQuery.message.id)
+
     if CallbackQuery.data == "refresh":
-        await py_runexec(
-            client, CallbackQuery.message.reply_to_message, CallbackQuery.message)
+        await py_runexec(client, message, replymsg)
 
 
 @Client.on_message(filters.command(["exec", "py"]) & dev_cmd)
 @ratelimiter
 async def py_exec(client, message):
-    """
-    Executes python command via bot with refresh button.
-    """
-
-    if len(message.command) < 2:
-        exec_usage = f"**Usage:** Executes python commands directly via bot.\n\n**Example: **<pre>/exec print('hello world')</pre>"
-        await message.reply_text(exec_usage)
-    else:
-        replymsg = await message.reply_text("executing..", quote=True)
-        await py_runexec(client, message, replymsg)
+	"""
+	Executes python command via bot with refresh button.
+	"""	
+	if len(message.command) < 2:
+		await message.reply_text( f"**Usage:** Executes python commands directly via bot.\n\n**Example: **<pre>/exec print('hello world')</pre>")				
+	else:
+	   replymsg = await message.reply_text("executing..", quote=True)
+	   await py_runexec(client, message, replymsg)
+    
