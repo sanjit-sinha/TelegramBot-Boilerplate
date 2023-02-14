5dccc3f16d97c2aecb0d584f807e18f451b96119
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-02-14 22:43:08 +0530

    Update config.py

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -10,10 +10,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 OWNER_USERID = json.loads(getenv("OWNER_USERID"))
 SUDO_USERID = OWNER_USERID
-try:
-    SUDO_USERID += json.loads(getenv("SUDO_USERID"))
-except:
-    pass
+try:SUDO_USERID += json.loads(getenv("SUDO_USERID"))
+except:pass
 SUDO_USERID = list(set(SUDO_USERID))
 
 MONGO_URI = getenv("MONGO_URI")
