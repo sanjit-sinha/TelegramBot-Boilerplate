812bdbf5d229b9d7fabcfd02a6805e501ebe6a2c
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-09-17 16:03:54 -0700

    Removed ratelimeter and useless decorators.

--- TelegramBot/helpers/decorators.py
+++ TelegramBot/helpers/decorators.py
@@ -1,56 +1,15 @@
-import asyncio
 from functools import wraps
-from cachetools import TTLCache
-from typing import Callable, Union
+from typing import Callable
 
 from pyrogram import Client
-from pyrogram.types import CallbackQuery, Message
+from pyrogram.types import Message
 
 from TelegramBot import loop
 from TelegramBot.helpers.functions import isAdmin
-from TelegramBot.helpers.ratelimiter import RateLimiter
-
-
-ratelimit = RateLimiter()
-
-# storing spammy user in cache for 1minute before allowing them to use commands again.
-warned_users = TTLCache(maxsize=128, ttl=60)
-warning_message = "Spam detected! ignoring your all requests for few minutes."
-
-
-def ratelimiter(func: Callable) -> Callable:
-    """
-    Restricts user's from spamming commands or pressing buttons multiple times
-    using leaky bucket algorithm and pyrate_limiter.
-    """
-
-    @wraps(func)
-    async def decorator(client: Client, update: Union[Message, CallbackQuery]):
-        userid = update.from_user.id
-        is_limited = await ratelimit.acquire(userid)
-
-        if is_limited and userid not in warned_users:
-
-            if isinstance(update, Message):
-                await update.reply_text(warning_message)
-                warned_users[userid] = 1
-                return
-
-            elif isinstance(update, CallbackQuery):
-                await update.answer(warning_message, show_alert=True)
-                warned_users[userid] = 1
-                return
-
-        elif is_limited and userid in warned_users: pass
-        else: return await func(client, update)
-
-    return decorator
 
 
 def admin_commands(func: Callable) -> Callable:
-    """
-    Restricts user's from using group admin commands.
-    """
+    """Restricts user's from using group admin commands."""
 
     @wraps(func)
     async def decorator(client: Client, message: Message):
@@ -60,13 +19,11 @@ def admin_commands(func: Callable) -> Callable:
     return decorator
 
 
-def errors(func: Callable) -> Callable:
-    """
-    Try and catch error of any function.
-    """
+def catch_errors(func: Callable) -> Callable:
+    """Try and catch error of any function."""
 
     @wraps(func)
-    async def decorator(client, message, *args,**kwargs):
+    async def decorator(client, message, *args, **kwargs):
         try:
             return await func(client, message, *args, **kwargs)
         except Exception as error:
@@ -87,10 +44,11 @@ def run_sync_in_thread(func: Callable) -> Callable:
     To use this decorator, apply it to any synchronous function, then you can then call that function to anywhere
     in your program and can use it along with await keyword. This will allow the function to be run asynchronously,
     and avoid blocking of the main event loop.
+    see example at :- https://github.com/sanjit-sinha/TelegramBot-Boilerplate/blob/43ce589fbf7fbfcba6347d0e1945ac7299667e80/TelegramBot/plugins/sudo/speedtest.py#L13
     """
 
     @wraps(func)
     async def wrapper(*args, **kwargs):
         return await loop.run_in_executor(None, func, *args, **kwargs)
-    
+
     return wrapper
