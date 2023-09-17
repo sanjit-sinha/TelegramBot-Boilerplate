04f17a4db1e7473738fa4fcc3645af2996c83a3e
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-09-17 15:57:32 -0700

    changed date format of saving chatids.

--- TelegramBot/database/database.py
+++ TelegramBot/database/database.py
@@ -1,25 +1,23 @@
-from TelegramBot.database import MongoDb as db
-from datetime import datetime, timezone
+from typing import Union
+from datetime import datetime
+
+from pyrogram.types import Message
+from TelegramBot.database.mongodb import users, chats
 
 
-async def saveUser(user):
-    """
-    Save the new user id in the database if it is not already there.
-    """
+async def save_user(user: Message) -> None:
+    """Saves the new user id in the database if it is not already there."""
 
     insert_format = {
         "name": (user.first_name or " ") + (user.last_name or ""),
         "username": user.username,
-        "date": datetime.now(timezone.utc)}
-    await db.users.update_document(user.id, insert_format)
+        "date": datetime.now()}
+
+    return await users.update_document(user.id, insert_format)
 
 
-async def saveChat(chatid):
-    """
-    Save the new chat id in the database if it is not already there.
-    """
-    
-    insert_format = {"date": datetime.now(timezone.utc)}
-    await db.chats.update_document(chatid, insert_format)
+async def save_chat(chatid: Union[int, str]) -> None:
+    """Save the new chat id in the database if it is not already there."""
 
-        
+    insert_format = {"date": datetime.now()}
+    return await chats.update_document(chatid, insert_format)
